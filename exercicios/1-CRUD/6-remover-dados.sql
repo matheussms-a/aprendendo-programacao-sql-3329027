28f2690f7fab7a460af41fcbf7cb7af318faf988
matheussms-a <matheussms@estudante.ufscar.br>
2026-01-06 14:05:27 -0300

    Modify SQL script to include SELECT before DELETE
    Added a SELECT statement to view invoices before deletion.

--- exercicios/1-CRUD/6-remover-dados.sql
+++ exercicios/1-CRUD/6-remover-dados.sql
@@ -1,4 +1,12 @@
 -- Da tabela invoice, remova o invoice cujo o InvoiceID corresponda ao valor 33
 
+SELECT *
+FROM invoices;
+
+DELETE FROM invoices
+WHERE Invoiceid is 33;
 
 -- Da tabela invoice, remova todos os invoices cujo o Total seja menor do que 1
+
+DELETE FROM invoices
+WHERE Total < 1;
