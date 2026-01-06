2cea81509e9d6715b4adb6ef14dec4210ad4a362
matheussms-a <matheussms@estudante.ufscar.br>
2026-01-06 14:05:56 -0300

    Enhance SQL query with calculations and filters
    Added arithmetic operations and WHERE clause to SQL query.

--- exercicios/2-construindo-consultas/1-operadores.sql
+++ exercicios/2-construindo-consultas/1-operadores.sql
@@ -1,6 +1,26 @@
 -- Construa uma consulta para a tabela "invoice_items" que retorne apenas as colunas InvoiceId, TrackId e UnitPrice.
 
+SELECT InvoiceId,
+TrackId,
+UnitPrice
+FROM invoice_items;
+
 -- Inclua nessa consulta mais três colunas, cada uma como resultado de uma operação aritmética diferente
+
+SELECT InvoiceId,
+TrackId,
+(UnitPrice + 1) * 1.3,
+(UnitPrice + 1) * 1.4,
+(UnitPrice + 1) * 1.5
+FROM invoice_items;
 
 -- Incremente essa consulta utilizando a cláusula WHERE, a qual restringirá o resultado apenas para linhas cujo o InvoiceId seja maior do que 20 e menor ou igual a 30
 
+SELECT InvoiceId,
+TrackId,
+(UnitPrice + 1) * 1.3,
+(UnitPrice + 1) * 1.4,
+(UnitPrice + 1) * 1.5 
+FROM invoice_items
+WHERE InvoiceId > 3
+AND InvoiceId != 10;
