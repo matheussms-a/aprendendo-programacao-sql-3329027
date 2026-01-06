5e0c8f09cbb86e8bcbcd915a5449517b27139624
matheussms-a <matheussms@estudante.ufscar.br>
2026-01-06 14:04:01 -0300

    Add salary classes to salario_classe table
    Inserted multiple salary classes into the salario_classe table.

--- exercicios/1-CRUD/3-inserir-dados.sql
+++ exercicios/1-CRUD/3-inserir-dados.sql
@@ -1,4 +1,17 @@
 -- Insira 3 classes salariais na tabela salario_classe
 
+INSERT INTO salario_classe(id_salario, salario, nivel)
+VALUES
+(1, 3500, 'estagiária'),
+(2, 4700, 'iniciante - grade 1'),
+(3, 5300, 'iniciante - grade 2');
+
+SELECT * FROM salario_classe;
 
 -- Insira 1 nova classe salarial, sem declarar o atributo id_salario
+
+INSERT INTO salario_classe(salario, nivel)
+VALUES
+(18000, 'MAX'),
+(8000, 'SUBGER'),
+(10900, 'GER');
