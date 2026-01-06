4d5eeee0ffa453855d0ae186330ae790eae24b37
matheussms-a <matheussms@estudante.ufscar.br>
2026-01-06 14:06:40 -0300

    Update 3-condicional.sql

--- exercicios/2-construindo-consultas/3-condicional.sql
+++ exercicios/2-construindo-consultas/3-condicional.sql
@@ -1,2 +1,21 @@
 -- Escreva uma consulta que retorne o nome por extenso dos estados brasileiros
 
+SELECT * FROM customers;
+
+SELECT DISTINCT
+Country as pais,
+State as estado
+FROM customers
+WHERE Country is 'Brazil';
+
+SELECT DISTINCT
+Country as pais,
+State as estado,
+CASE 
+ WHEN State = 'SP' THEN 'São Paulo'
+ WHEN State = 'RJ' THEN 'Rio de Janeiro'
+ WHEN State = 'DF' THEN 'Distrito Federal'
+ ELSE 'Estado desconhecido'
+END AS estado
+FROM customers
+WHERE Country LIKE 'Brazil'
